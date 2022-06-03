25958ac9da1f4ec8faca882656819ee5b3995864
Robert Benjamin <robert.benjamin@vimeo.com>
2022-06-03 19:04:00 -0400

    Add method tests and code

--- 5_struct_method_interface/struct_method_interface.go
+++ 5_struct_method_interface/struct_method_interface.go
@@ -1,1 +1,28 @@
 package structmethodinterface
+
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
--- 5_struct_method_interface/struct_method_interface_test.go
+++ 5_struct_method_interface/struct_method_interface_test.go
@@ -1,1 +1,42 @@
 package structmethodinterface
+
+import "testing"
+
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+}
+
+func TestPerimeter(t *testing.T) {
+	perimeterTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{10.0, 10.0}, 40.0},
+		{Circle{10}, 62.83185307179586},
+	}
+
+	for _, tt := range perimeterTests {
+		got := tt.shape.Perimeter()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+}
